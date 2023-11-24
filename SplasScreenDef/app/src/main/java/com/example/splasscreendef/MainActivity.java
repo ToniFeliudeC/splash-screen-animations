c5b1d73814821899f6fdf0a4a96832d2ddd6c304
ToniFeliu <91744505+ToniFeliudeC@users.noreply.github.com>
2023-11-24 20:23:24 +0100

    initial commit

---
+++ SplasScreenDef/app/src/main/java/com/example/splasscreendef/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.splasscreendef;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo1.startAnimation(fade1);
+
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        LayoutAnimationController controller =
+                new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop the animation
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        logo1.clearAnimation();
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        logo2.clearAnimation();
+        // ... stop other animations
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
+    }
+
+}
